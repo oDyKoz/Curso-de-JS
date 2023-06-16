9f0b8b66213ce4fcb5ea13f361fe023d52d8e5b9
oDyKoz <124197980+oDyKoz@users.noreply.github.com>
2023-06-16 19:06:30 -0300

    NEW

---
+++ Modulo B/index.js
@@ -0,0 +1,52 @@
+
+/*                  Aula 6
+ No js tem duas formas de comentarios, como (//) duas barras e (/*) barra e asterisco, mas cada um deles tem uma utilidade unica como o (//) que serve para ser utilizado para especificar que ele é utilizado somente para uma linha de comentario e o (/*) serve para varios comentarios 
+
+ Mas o assunto não é comentario mais sim variaveis no JS. Eles são slots de memoria para conduzir cada comando unico e proprio no JS.
+
+ - Identificadores 
+ >Podem começar com letra, $ ou _
+ 
+ >Não podem começar com numeros
+
+ >É possivel usar letras ou numeros
+
+ >É possivel usar acentos e simbolos
+
+ >Não podem conter espaços
+
+ >Não podem ser palavras reservadas
+
+ E vou utilizar o Node.js instalado no computador para praticar as variaveis do JS
+
+ -Dicas
+
+ >Maiúsculas e minúsculas fazer diferença
+
+ >Tente escolher nomes coerentes para as variáveis
+
+ >Evitar se tornar um "programador alfabeto"
+ ou "programador contador"
+
+ Avisando que todos os tipos númericos(de qualquer forma sendo numero inteiro ou quebrado) são considerados como 'number' e as palavras com ("") ('') (``) são 'strings' e o 'true' e 'false' são considerados 'boolean'.
+
+ -Data Types 
+
+>typeof
+
+ >number
+    -Infinity
+    -NaN
+>string
+>boolean
+>null
+>undenfined
+>object
+    -Array
+>function
+ */
+
+/*
+
+ */
+
